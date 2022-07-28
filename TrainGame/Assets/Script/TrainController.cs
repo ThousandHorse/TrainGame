89d2026546c3aeba94d38d2811b7b4a17e7d266c
TakumaChiba <takuma.chiba@playnext-lab.co.jp>
2022-07-28 12:00:04 +0900

    ・背景の追加 ・人、電車の動きを追加

---
+++ TrainGame/Assets/Script/TrainController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainController : MonoBehaviour
+{
+    private int frameCounter = 0;
+    private float trainTransform = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, trainTransform, 0);
+        frameCounter++;
+
+        if (frameCounter == 120)
+        {
+            frameCounter = 0;
+            trainTransform *= -1;
+
+        }
+    }
+}
